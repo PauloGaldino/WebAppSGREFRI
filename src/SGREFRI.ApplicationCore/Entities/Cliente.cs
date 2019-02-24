6d410e9a592e2b72208496b68a6b8028f3c0a64e
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2019-02-24 13:09:58 -0300

    Adicionar arquivos de projeto.

---
+++ src/SGREFRI.ApplicationCore/Entities/Cliente.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SGREFRI.ApplicationCore.Entities
+{
+   public class Cliente
+    {
+        public Cliente()
+        {
+                
+        }
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+    }
+    
+}
